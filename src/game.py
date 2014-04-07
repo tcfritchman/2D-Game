e908aad9749f33034fbed892aea64b99eee2945f
Thomas Fritchman <tcfritchman@gmail.com>
2014-04-07 01:03:58 -0700

    Various improvements, add timer platform

--- src/game.py
+++ src/game.py
@@ -14,7 +14,7 @@ def main():
     # Fill background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((50, 180, 180))
+    background.fill((150,200,255))
 
     # Blit to screen
     screen.blit(background, (0, 0))
@@ -25,9 +25,24 @@ def main():
     player = Player()
     platform_generator = PlatformGenerator()
 
+    # TEST PLATFORMS -------------------
+    testplatform1 = Platform()
+    testplatform1.set_position(0, 400)
+    testplatform2 = TimerPlatform()
+    testplatform2.set_position(125, 400)
+    
+
     # Object groups
-    allsprites = pygame.sprite.Group((player))
+    allsprites = pygame.sprite.Group((player, testplatform1, testplatform2))
+    platforms = pygame.sprite.Group((testplatform1, testplatform2))
     colliders = pygame.sprite.Group()
+    deadly = pygame.sprite.Group()
+    flicker = pygame.sprite.Group()
+    moving = pygame.sprite.Group()
+    timer = pygame.sprite.Group()
+    trampoline = pygame.sprite.Group()
+
+    font = pygame.font.Font(None, 18)
 
     clock = pygame.time.Clock()
 
@@ -46,21 +61,48 @@ def main():
                     player.move_left()
                 if event.key == K_SPACE:
                     player.jump()
+                # FOR TESTING
+                if event.key == K_ESCAPE:
+                    main()
             elif event.type == KEYUP:
                 if event.key == K_RIGHT or event.key == K_LEFT:
                     player.move_none()
                 if event.key == K_SPACE:
                     player.stop_jump()
 
-	new_platform = platform_generator.update()
-	if (new_platform):
-	    colliders.add(new_platform)
-	    
+        # Generate platforms
+        new_platform = platform_generator.update()
+        if (new_platform):
+            allsprites.add(new_platform)
+            platforms.add(new_platform)
+
+        # Group collidable platforms
+        colliders.empty()
+        #colliders.add(testplatform1)
+        #colliders.add(testplatform2)
+        for platform in platforms:
+            if platform.collides():
+                colliders.add(platform)
+
 
         allsprites.update()
         player.detect_collisions(colliders)
 
+        # Test output
+        text = font.render(player.to_string(), 1, ((255,0,0)))
+
+        # Make sure player hasn't left the screen
+        pos = player.get_position()
+        if pos[0] < 0 or pos[1] < 0 or pos[0] > WINDOWWIDTH or pos[1] > WINDOWHEIGHT:
+            # Reset the player (TEMPORARY)
+            print("test")
+            allsprites.remove(player)
+            player = Player()
+            allsprites.add(player)
+            
+        
         screen.blit(background, (0, 0))
+        screen.blit(text, (10, 10))
         allsprites.draw(screen)
         pygame.display.flip()
 
--- src/objects.py
+++ src/objects.py
@@ -7,10 +7,13 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_image('guy16.png', (0,0,0))
-	self.last_rect = self.rect
+        self.last_rect = self.rect
         self.movement = 'none'
+        self.friction = AIR_FRICTION
+        self.move_force = AIR_MOVE_FORCE
         self.jumping = False
-        #self.frame_time = 1 / FPS
+        self.initial_jump = True
+        self.flytime = 0
         self.xforce = 0
         self.yforce = 0
         self.xvelocity = 0
@@ -19,38 +22,66 @@ class Player(pygame.sprite.Sprite):
         self.yposition = 0
         self.bottom_collision = False
 
+    def to_string(self):
+        return 'xforce: {:8f}, yforce: {:8f}, xvelocity: {:8f}, yvelocity: {:8f}'.format(self.xforce, self.yforce, self.xvelocity, self.yvelocity)
+
     def update(self):
+        # Determines all character physics
+
+        if self.bottom_collision:
+            self.move_force = PLATFORM_MOVE_FORCE
+            self.friction = PLATFORM_FRICTION
+            self.yforce = 0
+            self.yvelocity = 0
+        else:
+            self.move_force = AIR_MOVE_FORCE
+            self.friction = AIR_FRICTION
+            self.yforce = GRAVITY
+
         # Calculate forces (factors in gravity)
         if self.movement == 'none':
             self.xforce = 0
         elif self.movement == 'right':
-            self.xforce = MOVE_FORCE
+            self.xforce = self.move_force
         elif self.movement == 'left':
-            self.xforce = -MOVE_FORCE
+            self.xforce = -self.move_force
 
-        if self.bottom_collision:
-            self.yforce = 0
-            self.yvelocity = 0
-        else:
-            self.yforce = GRAVITY
-        
+        # Add in platform force when on a platform
+        #if self.bottom_collision:
+        #    self.xforce += (PLATFORM_SPEED / self.friction) - self.xvelocity
+
+        # Jump button is being held. Apply inital jump force then 'fly' force
         if self.jumping:
-            self.yforce += JUMP_FORCE
+            if self.initial_jump:
+                self.yforce = JUMP_FORCE
+            #elif self.flytime < FLY_TIME:
+            #    self.flytime += 1
+            #    self.yforce += FLY_FORCE
+            self.initial_jump = False
+        else:
+            self.initial_jump = True
+            #self.flytime = 0
 
         # Calculate velocity (factors in friction)
-        self.xvelocity = FRICTION * (self.xvelocity + self.xforce)
         self.yvelocity = self.yvelocity + self.yforce
+        self.xvelocity = self.friction * (self.xvelocity + self.xforce)
 
-	if self.bottom_collision:
-	    self.xvelocity = PLATFORM_SPEED + (FRICTION * (self.xvelocity + self.xforce))
+        # Bound the Y velocity
+        if self.yvelocity < MIN_VELOCITY_Y:
+            self.yvelocity = MIN_VELOCITY_Y
+        elif self.yvelocity > MAX_VELOCITY_Y:
+            self.yvelocity = MAX_VELOCITY_Y
 
         # Calculate new position
         self.xposition = self.xposition + self.xvelocity
         self.yposition = self.yposition + self.yvelocity
 
         # Update position and save last position
-	self.last_rect = self.rect.copy()
+        self.last_rect = self.rect.copy()
         self.rect.topleft = (self.xposition, self.yposition)
+
+    def get_position(self):
+        return self.rect.topleft
 
     def force(self, amount):
         # Updates the force on self
@@ -99,12 +130,15 @@ class Player(pygame.sprite.Sprite):
             # right line cross
             if self.rect.right > collider.rect.left:
                 R = True
-            if T and B and L and R:	# A collision has occurred
-		if self.rect.y >= self.last_rect.y and self.last_rect.bottom <= collider.rect.top:
-		    # A bottom collision has occured
+            if T and B and L and R: # A collision has occurred
+                if self.rect.y >= self.last_rect.y and self.last_rect.bottom <= collider.rect.top:
+                    # A bottom collision has occured
                     self.bottom_collision = True
                     self.rect.bottom = collider.rect.top
-        
+
+                    # Determine what we've collided with and act accordingly
+                    if collider.platform_type == "Timer":
+                        collider.countdown()
 
 class TestPlatform(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -116,33 +150,80 @@ class TestPlatform(pygame.sprite.Sprite):
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self):
-	pygame.sprite.Sprite.__init__(self)
-	self.image = pygame.Surface([120, 25])
-	self.image.fill((0, 0, 0))
-	self.rect = self.image.get_rect()
+        pygame.sprite.Sprite.__init__(self)
+        self.platform_type = "Platform"
+        self.image = pygame.Surface([120, 25])
+        self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.collidable = True
+        self.deadly = False
+        self.trampoline = False
+        self.hastimer = False
     
     def update(self):
-	self.rect.x += PLATFORM_SPEED
+        #self.rect.x += PLATFORM_SPEED
+        pass
 
     def set_position(self, x, y):
-	self.rect.topleft = (x, y)
+        self.rect.topleft = (x, y)
+
+    def collides(self):
+        if self.collidable:
+            return True
+        else:
+            return False
+
+class TimerPlatform(Platform):
+    def __init__(self):
+        Platform.__init__(self)
+        self.platform_type = "Timer"
+
+        self.visible = pygame.Surface([120, 25])
+        self.visible.fill((255, 128, 0))
+        self.invisible = pygame.Surface([120,25], pygame.SRCALPHA, 32)
+        self.invisible.convert_alpha()
+
+        self.image = self.visible
+        #self.image = pygame.Surface([120, 25])
+        #self.image.fill((255, 128, 0))
+        self.timer_ticking = False
+        self.timer_val = 0
+
+    def update(self):
+        if self.timer_ticking:
+            self.timer_val += 1
+
+            # Blink
+            if (self.timer_val / 4) % 2 == 1:
+                self.image = self.invisible
+            else:
+                self.image = self.visible
+                
+            if self.timer_val >= TIMER_FRAMES:
+                self.collidable = False
+                self.timer_ticking = False
+        Platform.update(self)
+
+    def countdown(self):
+        if self.timer_ticking == False:
+            self.timer_ticking = True
 
 class PlatformGenerator:
     def __init__(self):
         self.framecounter = 0
-	self._framemax = (PLATFORM_WIDTH + PLATFORM_GAP) / abs(PLATFORM_SPEED) 
+        self._framemax = (PLATFORM_WIDTH + PLATFORM_GAP) / abs(PLATFORM_SPEED) + 1 + 3
 
-    def update():
-	if self.framecounter == self._framemax:
-	    # Generate a new platform
-	    plat = Platform()
-	    plat.set_position(WINDOWWIDTH + 20, WINDOWHEIGHT - 50)
-	    self.framecounter = 0
-	    self.framecounter += 1
-	    return plat()
-    	else:
-	    self.framecounter += 1
-	    return False
-	
-	
+    def update(self):
+        if self.framecounter == self._framemax:
+            # Generate a new platform
+            plat = Platform()
+            plat.set_position(WINDOWWIDTH + 20, WINDOWHEIGHT - 50)
+            self.framecounter = 0
+            self.framecounter += 1
+            return plat
+        else:
+            self.framecounter += 1
+            return False
+    
+    
 
